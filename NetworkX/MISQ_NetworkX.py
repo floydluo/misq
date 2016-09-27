1ef9aae6ae63306e05dbf16de1619a1cad24cba9
floyd <114020252@link.cuhk.edu.cn>
2016-09-27 22:12:58 +0800

    The dynamic version

--- NetworkX/MISQ_NetworkX.py
+++ NetworkX/MISQ_NetworkX.py
@@ -5,6 +5,8 @@ from pandas import DataFrame, Series
 import networkx as nx
 import itertools
 import sqlite3
+from forcenetwork import DrawNetGraph
+
 
 con = sqlite3.connect("data.sqlite")
 
@@ -136,137 +138,13 @@ for subg in legendary:
     Legendary.add_edges_from(subg.edges(data = True))
     Legendary.add_nodes_from(subg.nodes(data = True))
 
-def DrawGraph(Achievement, layout = "random"):
-    if layout == "random":
-        pos = nx.random_layout(Achievement)
-    if layout == "shell":
-        pos = nx.shell_layout(Achievement)
-    if layout == "spring":
-        pos = nx.spring_layout(Achievement)
-    if layout == "spectral":
-        pos = nx.spectral_layout(Achievement)
-
-    # for nodes
-    auth1 = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] ==1]
-    auth2 = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] ==2]
-    auth3 = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] ==3]
-    auth4 = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] ==4]
 
 
-    auth5_10  = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] >=5  and d["paper_num"] < 11 ]
-    auth11_20 = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] >=11 and d["paper_num"] < 21 ]
-    auth21_30 = [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] >=21 and d["paper_num"] < 31 ]
-
-    auth31_   =  [ auth for (auth, d) in Achievement.nodes(data = True) if d["paper_num"] >=31]
-
-
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth1, node_size=50)
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth2, node_size=100, node_color = 'g')
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth3, node_size=200, node_color = 'y')
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth4, node_size=300, node_color = 'b')
-
-
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth5_10,  node_size=400, node_color = 'c')
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth11_20, node_size=600, node_color = 'm')
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth21_30, node_size=800, node_color = 'k')
-
-    nx.draw_networkx_nodes(Achievement,pos,nodelist = auth31_, node_size=1000, node_color = 'k')
-
-
-    # for edges
-    edge1 = [ (u,v) for (u,v,d) in Achievement.edges(data = True) if d["freq"] == 1]
-    edge2 = [ (u,v) for (u,v,d) in Achievement.edges(data = True) if d["freq"] == 2]
-    edge3 = [ (u,v) for (u,v,d) in Achievement.edges(data = True) if d["freq"] == 3]
-
-
-    nx.draw_networkx_edges(Achievement, pos, edgelist = edge1, width = 1, edge_color = "grey")
-    nx.draw_networkx_edges(Achievement, pos, edgelist = edge2, width = 3,  edge_color = 'b')
-    nx.draw_networkx_edges(Achievement, pos, edgelist = edge3, width = 10, edge_color = "m")
-
-    # for labels
-    names = {}
-    for v, d in Achievement.nodes(data = True):
-        names[v] = d["full_name"]
-    nx.draw_networkx_labels(Achievement,pos,names,font_size=2, color ="w")
-
-
-
-
-# first blood
-DrawGraph(First_Blood)
-plt.savefig("First_Blood1", dpi = 700)
-plt.clf()
-
-DrawGraph(First_Blood, layout = "spring")
-plt.savefig("First_Blood2", dpi = 700)
-plt.clf()
-
-# double kill
-DrawGraph(Double_Kill)
-plt.savefig("Double_Kill1", dpi = 700)
-plt.clf()
-
-DrawGraph(Double_Kill, layout = "spring")
-plt.savefig("Double_Kill2", dpi = 700)
-plt.clf()
-
-
-
-# triple kill
-DrawGraph(Triple_Kill)
-plt.savefig("Triple_Kill1", dpi = 700)
-plt.clf()
-
-DrawGraph(Triple_Kill, layout = "spring")
-plt.savefig("Triple_Kill2", dpi = 700)
-plt.clf()
-
-
-
-# quadra kill
-DrawGraph(Quadra_Kill)
-plt.savefig("Quadra_Kill1", dpi = 700)
-plt.clf()
-
-DrawGraph(Quadra_Kill, layout = "spring")
-plt.savefig("Quadra_Kill2", dpi = 700)
-plt.clf()
-
-# penta kill
-DrawGraph(Penta_Kill)
-plt.savefig("Penta_Kill1", dpi = 700)
-plt.clf()
-
-DrawGraph(Penta_Kill, layout = "spring")
-plt.savefig("Penta_Kill2", dpi = 700)
-plt.clf()
-
-
-# rampage
-DrawGraph(Rampage)
-plt.savefig("Rampage1", dpi = 700)
-plt.clf()
-
-DrawGraph(Rampage, layout = "spring")
-plt.savefig("Rampage2", dpi = 700)
-plt.clf()
-
-
-# godlike
-DrawGraph(Godlike)
-plt.savefig("Godlike1", dpi = 700)
-plt.clf()
-
-DrawGraph(Godlike, layout = "spring")
-plt.savefig("Godlike2", dpi = 700)
-plt.clf()
-
-
-# legendary
-DrawGraph(Legendary)
-plt.savefig("Legendary1", dpi = 700)
-plt.clf()
-
-DrawGraph(Legendary, layout = "spring")
-plt.savefig("Legendary2", dpi = 700)
-plt.clf()
+DrawNetGraph(First_Blood, "Graphs/1_first")
+DrawNetGraph(Double_Kill, "Graphs/2_double")
+DrawNetGraph(Triple_Kill, "Graphs/3_triple")
+DrawNetGraph(Quadra_Kill, 'Graphs/4_quadra')
+DrawNetGraph(Penta_Kill, "Graphs/4_penta")
+DrawNetGraph(Rampage, "Graphs/5_rampage")
+DrawNetGraph(Godlike, "Graphs/6_godlike")
+DrawNetGraph(Legendary,"Graphs/7_legendary")
